f05cf7d1de2daa438914b7d559ea6a23cf23c8e9
Anveshan <akunduru4@gmail.com>
2021-06-05 20:25:58 +0530

    Add files via upload

---
+++ React JS/meet-up-app/src/pages/Favorites.js
@@ -0,0 +1,6 @@
+function FavoritesPage(){
+    return <div> This is Favorites Page</div>;
+}
+
+
+export default  FavoritesPage;
---
+++ React JS/meet-up-app/src/pages/NewMeetup.js
@@ -0,0 +1,30 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { useHistory } from "react-router-dom";
+
+function NewMeetupPage() {
+  const history = new useHistory();
+
+  //fetch default is a GET method. We need to configure it to POST , to submit data
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-8c79a-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() =>  {
+      history.replace('/');
+    });
+  }
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupPage;
